c71cabb9c5fe1e9f96003616a92542cc7f6c2f0c
Alessandro Amella <44754837+alessandroamella@users.noreply.github.com>
2025-08-05 18:17:00 +0200

    robe

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run frontend and backend concurrently
-concurrently "cd ./frontend && pnpm run dev" "cd ./backend && pnpm run start:dev"
+pnpx concurrently "cd ./frontend && pnpm run dev" "cd ./backend && pnpm run start:dev"
